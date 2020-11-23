9aae9e7c676ec1c07f36d136f7f792b7ffa39c4b
ludomitch <lmitchener@interrodata.com>
2020-11-23 18:51:15 +0000

    weak cons workingish

---
+++ neuro/weak_learned.py
@@ -0,0 +1,106 @@
+from animalai.envs.gym.environment import AnimalAIGym
+from centroidtracker import CentroidTracker
+
+from macro_action import MacroAction
+from weak_logic import Logic
+from utils import preprocess, object_types
+
+class Pipeline:
+    def __init__(self, args):
+        self.args = args
+        self.ct = None
+        self.gg_id = 0
+        env_path = args.env
+        worker_id = 1
+        seed = args.seed
+        self.arenas = args.arena_config
+        self.buffer_size = 30
+        first_arena = self.arenas[0] if self.arenas is not None else None
+
+        # ac = ArenaConfig(arena_path)
+        # Load Unity environment based on config file with Gym or ML agents wrapper
+        self.env = AnimalAIGym(
+            environment_filename=env_path,
+            worker_id=worker_id,
+            n_arenas=1,
+            arenas_configurations=first_arena,
+            seed=seed,
+            grayscale=False,
+            resolution=84,
+            inference=args.inference
+        )
+        self.logic = Logic(self.buffer_size)
+
+    def comp_stats(self):
+        pass
+
+    def format_macro_results(self, stats):
+        res = """
+        Success: {success}
+        Micro steps taken: {micro_step}
+        Total reward: {reward}
+        Checks satisfied: {checks}
+        """.format(
+            **stats
+        )
+        return res
+
+    def take_macro_step(self, env, state, step_results, macro_action, pass_mark=0):
+        ma = MacroAction(env, self.ct, state, step_results, macro_action)
+        # print(f"Initiating macro_action: {macro_action['initiate']}")
+        step_results, state, macro_stats, micro_step = ma.run(pass_mark)
+        # print(f"Results: {self.format_macro_results(macro_stats)}")
+        return step_results, state, micro_step, macro_stats["success"]
+
+    def episode_over(self, done):
+        if done:
+            return True
+        return False
+
+    def learn_run(self):
+        success_count = 0
+        choice = 'random'
+        traces = [] # list of lists: [actions, observables, success, macro_steps]
+        for idx in range(self.args.num_episodes):
+            self.env.reset(self.arenas[0])
+            # print(f"======Running episode {idx}=====")
+            step_results = self.env.step([[0, 0]])  # Take 0,0 step
+            global_steps = 0
+            macro_step = 0
+            reward = 0
+
+            self.ct = {ot: CentroidTracker() for ot in object_types} # Initialise tracker
+            actions_buffer = []
+            observables_buffer = []
+            if (idx%self.buffer_size==0)&(idx!=0):
+                choice = 'ilasp'
+                self.logic.ilasp.generate_examples(traces)
+                self.logic.update_learned_lp()
+
+            while not self.episode_over(step_results[2]):
+                if global_steps >= 1000:
+                    success = False
+                    print("Exceeded max global steps")
+                    break
+                state = preprocess(self.ct, step_results, global_steps, reward)
+                macro_action, observables = self.logic.run(
+                    macro_step,
+                    state,
+                    choice=choice)
+                step_results, state, micro_step, success = self.take_macro_step(
+                    self.env, state, step_results, macro_action
+                )
+                global_steps += micro_step
+                macro_step +=1
+                actions_buffer.append(macro_action['raw'][0])
+                observables_buffer.append(observables)
+
+            traces.append([actions_buffer, observables_buffer, success, macro_step])
+            # nl_success = "Success" if success else "Failure"
+            # print(f"Episode was a {nl_success}")
+            success_count += success
+
+        print(
+            f"Final results: {success_count}/{self.args.num_episodes} episodes were completed successfully"
+        )
+        self.env.close()
---
+++ neuro/weak_logic.py
@@ -0,0 +1,331 @@
+from collections import deque, namedtuple
+import random as rnd
+import subprocess
+
+from clyngor import ASP
+
+from utils import get_overlap, get_distance
+
+AnswerSet = namedtuple('AS', ['r', 'p', 'a']) # r for raw, p for parsed, a for arity
+parse_args = lambda x: list(list(ASP(x+'.').parse_args)[0])[0]
+sample_vars = ["X", "Y", "Z"]
+macro_actions = {
+    "explore":3,
+    "interact":1,
+    "rotate":0
+}
+valid_observables = {
+    'present',
+    'visible',
+    'on',
+    'adjacent',
+    'goal',
+    'goal1',
+    'wall',
+    'platform',
+    'red'
+}
+
+
+class Grounder:
+    def __init__(self):
+        pass
+    # @staticmethod
+    # def in_front(macro_step,state):
+    #     in_front = ""
+    #     for bbox, _, _, _id in state['obj']:
+    #         for bbox1, _, _, _id1 in state['obj']:
+    #             dist = get_distance(bbox, bbox1)
+    #             if (_id1!=_id)&(dist<0.02):
+    #                 in_front += f"adjacent({_id},{_id1}, {macro_step}).\n"
+    #     return in_front
+    @staticmethod
+    def adjacent(macro_step,state):
+        adjacent = ""
+        for bbox, _, _, _id in state['obj']:
+            for bbox1, _, _, _id1 in state['obj']:
+                dist = get_distance(bbox, bbox1)
+                if (_id1!=_id)&(dist<0.01):
+                    adjacent += f"adjacent({_id},{_id1}, {macro_step}).\n"
+        return adjacent
+    @staticmethod
+    def timestep(macro_step,_):
+        timestep = f"timestep({macro_step}).\n"
+        return timestep
+    @staticmethod
+    def on(macro_step,state):
+        on = ""
+        bottom_rect = [0, 0.75, 1, 0.25]
+        for bbox, _, _, _id in state['obj']:
+            if get_overlap(bbox, bottom_rect)>0.5:
+                on += f"on(agent,{_id},{macro_step}).\n"
+        return on
+
+    @staticmethod
+    def visible(macro_step,state):
+        visible = ""
+        for box, obj_type, _occ_area, _id in state['obj']:
+            visible += f"visible({_id},{int(box[3]*1000)},{macro_step}).\n"
+            if obj_type!="goal":
+                visible +=f"{obj_type}({_id}).\n"
+        return visible
+    @staticmethod
+    def goal_visible(_,state):
+        try:
+            gg_id = next(i[3] for i in state['obj'] if i[1] in ['goal', 'goal1'])
+        except StopIteration:
+            gg_id = 42
+        return f"goal({gg_id}).\n"
+    def run(self, macro_step, state):
+        res = ""
+        for k,v in vars(Grounder).items():
+            if isinstance(v, staticmethod):
+                res+= getattr(self, k)(macro_step,state)
+        return res
+
+class Clingo:
+    def __init__(self):
+        self.macro_actions_learned = []
+
+    def random_action_grounder(self, macro_step, ground_observables):
+        lp = f"""
+            {ground_observables}
+            present(X,T):-goal(X), timestep(T).
+            present(X,T):- visible(X, _, T).
+            initiate(explore(X,Y,O),T):- visible(X, O, T), present(Y,T), X!=Y.
+            initiate(interact(X),T):-visible(X,_,T).
+            initiate(rotate,{macro_step})."""
+        res = self.asp(lp)
+        filtered_mas = [i for i in res.r[0] if (
+            'initiate' in i)&(not any(j in i for j in self.macro_actions_learned))]
+        rand_action = rnd.choice(filtered_mas)
+        # print([i for i in res.r[0] if 'initiate' in i])
+        return [[rand_action]]
+
+    @staticmethod
+    def asp(lp):
+        as1 = list(ASP(lp).atoms_as_string)
+        as2 = list(ASP(lp).parse_args)
+
+        return AnswerSet(r=as1, p=as2, a=len(as1))
+
+
+    def macro_processing(self, answer_set):
+        # Look for initiate
+        res = {
+            'initiate':[],
+            'check':[],
+            'raw':[]
+        }
+        if not isinstance(answer_set, list):
+            answer_set = answer_set.r
+        if len(answer_set)>1:
+            # print("More than 1 answer set so stopping at first initiate")
+            # print(answer_set)
+            _break = True
+        else:
+            _break = False
+        # print(answer_set)
+        for ans_set in answer_set:
+            for literal in ans_set:
+                if 'initiate' in literal:
+                    # From initiate(action(args),ts), select action(args)
+                    res['initiate'].append(parse_args(literal)[1][0])
+                    res['raw'].append(literal)
+                    if _break:
+                        break
+
+
+        # No action returned
+        if not res['initiate']:
+            # print("NO ACTION")
+            return False
+
+        # if two actions are returned from program then use random action.
+        if len(res['initiate'])>1:
+            # print("More than one action")
+            # print(res['initiate'])
+            res['raw'] = "multi:"+str(res['initiate'])
+            res['initiate'] = [rnd.choice(res['initiate'])]
+            # return False
+
+        # Add checks for chosen action
+        checks = list(ASP(f"""            
+            {res['raw'][0]}.
+            check(visible, Y):- initiate(explore(X,Y,Z),T).
+            check(time, 250):- initiate(explore(X,Y,Z),T).
+            check(time, 150):- initiate(interact(X),T).
+            check(time, 250):- initiate(avoid_red,T).
+            check(time, 50):- initiate(rotate,T).""").atoms_as_string)
+        checks = checks[0]
+        checks = [parse_args(i)[1] for i in list(checks) if 'check' in i]
+        res['check'] = checks
+        # print(res)
+        return res
+
+    def run(self, macro_step, lp, random=False):
+        # Just ground macro actions based on observables
+        if random:
+            res = self.random_action_grounder(macro_step, lp)
+        else: # Run full lp
+            # print(lp)
+            res = self.asp(lp)
+            # print(res)
+            # print(lp)
+                
+        return self.macro_processing(res)
+
+class Ilasp:
+    def __init__(self, memory_len=40):
+        # Examples are [int:weight, string:example]
+        self.memory_len = memory_len
+        self.examples = deque(maxlen=self.memory_len)
+        self.macro_actions_learned = []
+
+    def create_mode_bias(self):
+        return """
+#modeo(1, rotate).
+#modeo(1, interact).
+#modeo(1, explore).
+#modeo(1, goal(var(X))).
+#modeo(1, visible(var(X), var(Z), var(T))).
+#modeo(1, occludes(var(X),var(Y), var(T))).
+#weight(1).
+#weight(-1).
+#maxv(4).
+#maxp(1).
+"""
+
+    def extract_action(self, action):
+        res = action.split('initiate(')
+        res = res[0].split('(')[0]
+        return res
+    def expand_trace(self, trace, idf):
+        discount_factor = 0.9
+        actions, observables, success, len_trace = trace
+        success = 10 if success else -10
+        examples = []
+        values = []
+        # actions = []
+        for step in range(len_trace):
+            examples.append(f"#pos(a{idf+step},\n{{{actions[step]}}},\n{{}},\n{{{observables[step]}}}).\n")
+            values.append(success*discount_factor**step)
+            # actions.apennd(self.extract_action(actions))
+        return examples, values, len_trace #, actions
+    def generate_examples(self, traces):
+        examples = []
+        values = []
+        # actions = []
+        count = 0
+        for trace in traces:
+            e, v, c = self.expand_trace(trace, count)
+            count += c
+            examples += e
+            values += v
+            # actions += a
+        
+        # Get order
+        order = [i for (v, i) in sorted(((v, i) for (i, v) in enumerate(values)),reverse=True)]
+        order = [f"a{i}" for i in order]
+        pairs = [[order[i], order[i+1]] for i in range(0,len(order)-1)]
+        pairs = [f"#brave_ordering({', '.join([i[0], i[1]])})." for i in pairs]
+        ordering = "\n".join(pairs)
+        examples = ordering + "".join(examples)
+        self.examples = examples
+
+
+    def run(self, lp):        
+        # Create text file with lp
+        with open("tmp.lp", "w") as text_file:
+            text_file.write(lp+self.create_mode_bias() + self.examples)
+        # Start bash process that runs ilasp learning
+        bashCommand = "ilasp --version=2i tmp.lp -q"
+        process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+        if error:
+            raise Exception(f"ILASP error: {error.decode('utf-8')}")
+        
+        # Return new lp with learned rules
+        if bool(output): #learned rules
+            output = output.decode("utf-8")
+
+            if output:
+                print(f"NEW RULES LEARNED: {output}")
+            if output=="UNSATISFIABLE\n":
+                return False
+            for ma in macro_actions:
+                if ma in output:
+                    self.macro_actions_learned.append(ma)
+            return output
+        print("NO RULES LEARNED")
+        return False # No learned rules, will choose random macro
+        
+        
+class Logic:
+    def __init__(self, buffer_size = 40):
+        self.grounder = Grounder()
+        self.ilasp = Ilasp(buffer_size)
+        self.clingo = Clingo()
+        self.e = 1
+        self.e_discount = 8e-3
+        self.learned_lp = ""
+
+    def macro_kb(self):
+        """This is what we want to learn."""
+        return """
+            0{initiate(explore(X,Y,Z),T)}1:- visible(X,Z,T), occludes(X,Y,T).
+            initiate(interact(X),T):- visible(X, _,T), goal(X).
+            initiate(rotate,T):- not visible(T), timestep(T)."""
+    def main_lp(self):
+        return """
+present(X,T):-goal(X), timestep(T).
+% Observables rules
+present(X,T):- visible(X, _, T).
+visible(T):- visible(X, _, T).
+occlusion(Y):- visible(X,Y,T).
+object(X):- visible(X,Y,T).
+separator(Y, T):-on(agent, X, T), adjacent(X, Y, T), platform(X).
+not_occluding(X, T):-on(agent, X, T).
+not_occluding(X, T):-separator(X, T).
+occludes(X,Y,T) :- present(Y, T), visible(X, _, T), not visible(Y, _, T), not not_occluding(X, T).
+1{rotate,interact,explore}1.
+initiate(rotate, T):- timestep(T), rotate.
+initiate(interact(X), T):- timestep(T), object(X), interact.
+initiate(explore(X,Y,Z),T):-timestep(T), object(X), object(Y), occlusion(Z), explore.
+"""
+
+
+
+    def update_learned_lp(self):
+        rules_learned = self.ilasp.run(self.learned_lp)
+        if rules_learned:
+            self.learned_lp += rules_learned
+            # Sync two lists
+            self.clingo.macro_actions_learned = self.ilasp.macro_actions_learned
+
+    def update_examples(self, traces):
+        self.ilasp.generate_examples(traces)
+
+    def run(self, macro_step, state, choice='random'):
+        # Ground state into high level observable predicates
+        observables = self.grounder.run(macro_step, state)
+
+        if not self.learned_lp:
+            self.learned_lp = self.main_lp()
+
+
+        if choice == 'ilasp':
+            if self.learned_lp:
+                action = self.clingo.run(macro_step, self.learned_lp +observables, random=False)
+            else:
+                action = self.clingo.run(macro_step, observables, random=True)
+
+        elif choice == 'random':
+            action = self.clingo.run(macro_step, observables, random=True)
+        else:
+            raise Exception("Modality not recognised")
+        
+        if not action:
+            # print("No action choosing randomly")
+            action = self.clingo.run(macro_step, observables, random=True)
+        return action, observables

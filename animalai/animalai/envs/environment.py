ad899abb11c23150c6ba0b4bcd843af021847ef9
ludomitch <lmitchener@interrodata.com>
2020-11-26 16:31:26 +0000

    -

--- animalai/animalai/envs/environment.py
+++ animalai/animalai/envs/environment.py
@@ -832,9 +832,8 @@ class AnimalAIEnvironment(UnityEnvironment):
         self.ramp_config =rc(self.counter)
         ac = ArenaConfig(self.ramp_config)
         self.counter+=1
-        print("COUNTER", self.counter)
-        if self.counter % 5 == 0:
-            print(f"wallah: {self.counter}")
+        if self.counter % 500 == 0:
+            print(f"COUNTER: {self.counter}")
         arenas_configurations_proto = ac.to_proto()
         arenas_configurations_proto_string = arenas_configurations_proto.SerializeToString(
             deterministic=True

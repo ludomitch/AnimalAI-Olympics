7b31a2af46137cf69824bd7017d885d7a3ff221c
ludomitch <lmitchener@interrodata.com>
2020-12-09 22:16:51 +0100

    -

--- animalai/animalai/envs/slack_creator.py
+++ animalai/animalai/envs/slack_creator.py
@@ -1,5 +1,5 @@
 from collections import namedtuple as nt
-from random import randrange, choice
+from random import randrange, choice, choices
 import math
 
 vector = nt('vec', ['x', 'y', 'z'])
@@ -23,40 +23,42 @@ def make_obj(pos=False, size=False, name=False, rot=False):
         res += """
       colors:
       - !RGB {r: 0, g: 0, b: 255}"""
-    # if name == 'Wall':
-    #     res += """
-    #   colors:
-    #   - !RGB {r: 255, g: 0, b: 0}"""
+
     return res
 
 
 def run(counter):
-    wall_p = vector(choice([10,30]), 0, 20)
+    if counter%2==0:
+        side = 10
+    else:
+        side = 30
+    wall_p = vector(side, 0, randrange(10,20))
     if counter < 20:
         num_turns = 0
         wall_width = 8
-        wall_len = randrange(8,15)
-    elif counter < 200:
-        num_turns = 1
-        wall_width = randrange(5,10)
-        wall_len = randrange(8,25)
+        wall_len = randrange(7,10)
+    elif counter < 150:
+        num_turns = choices([0,1,2], weights=[0.1, 0.6,0.3])[0]
+        wall_width = randrange(3,10)
+        wall_len = randrange(8,min(25, ((wall_p.z-2)*2)))
 
     else:
-        num_turns = 2
+        num_turns = choices([0,1,2], weights=[0.05, 0.25,0.7])[0]
         wall_width = randrange(1,5)
-        wall_len = randrange(8,35)
+        wall_len = randrange(8,min(30, ((wall_p.z-2)*2)))
 
-    wall_s = vector(wall_width, 5, wall_len)
+    height = randrange(2,6)
+    wall_s = vector(wall_width, height, wall_len)
 
     deathzone_p = vector(20,0,20)
-    deathzone_s = vector(35,0,35)
+    deathzone_s = vector(38,0,38)
 
     base = """
 !ArenaConfig
 arenas:
   -1: !Arena
     pass_mark: 1
-    t: 250
+    t: 180 
     items:"""
     pos = wall_p
     goal_x = pos.x
@@ -65,7 +67,7 @@ arenas:
         if c==0: # horiz
             rot = 180
             wall_len = min(wall_len, (min(40-pos.x, pos.x)-4)*2)
-            size = vector(wall_len,5,wall_width)
+            size = vector(wall_len,height,wall_width)
             if wall_p.x >20: # going left
                 left = -1
                 pos = vector(pos.x-size.x/2, 0, pos.z+wall_s.z/2 + size.z/2)
@@ -79,7 +81,7 @@ arenas:
         else: # vertical
             rot = 0
             wall_len = max(wall_len, min(40-pos.z, pos.z)-4*2)
-            size = vector(wall_width,5,wall_len)
+            size = vector(wall_width,height,wall_len)
             if pos.z > 20: # going down
                 pos = vector(pos.x + left*prev_size.x/2-left*2, 0, pos.z - prev_size.z/2-size.z/2)
                 goal_x = pos.x
@@ -99,11 +101,13 @@ arenas:
         pos = inp['pos']
         prev_size = inp['size']
         base+=make_obj(**inp)
-    goodgoal_p = vector(goal_x, 5, goal_z)
+    goodgoal_p = vector(goal_x, height, goal_z)
     goal_s = randrange(1, 5)
     goodgoal_s = vector(goal_s,goal_s,goal_s)     
-    agent_p = vector(wall_p.x,5, wall_p.z)
-    agent_rot = round(math.degrees(math.atan2(agent_p.x - goal_x, agent_p.z-goal_z)) +180)
+    agent_p = vector(wall_p.x,height, wall_p.z)
+    # agent_rot = round(math.degrees(math.atan2(agent_p.x - goal_x, agent_p.z-goal_z)) +180)
+    agent_rot = 0
+
     for obj in ['Agent', 'Wall', "GoodGoal", "DeathZone"]:
         inp = {"name":obj}
         if obj.lower()+'_p' in locals():

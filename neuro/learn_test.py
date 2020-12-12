53e5aa1cf2d30d55b2d58caf607c4af218c19e47
ludomitch <lmitchener@interrodata.com>
2020-12-12 22:06:15 +0100

    -

--- neuro/learn_test.py
+++ neuro/learn_test.py
@@ -14,7 +14,7 @@ if __name__=="__main__":
 		f"../competition_configurations/{i}.yml" for i in list(cfg.COMPETITION_CONFIGS['Ramp Usage'])
 	]
 
-	num = len(arenas)*5
+	num = len(arenas)*10
 	args = margs(env=env_path, seed=1, arenas=arenas, num_episodes=num+1, inference=False)
 	pipe = Pipeline(args)
 	pipe.buffer_size = num

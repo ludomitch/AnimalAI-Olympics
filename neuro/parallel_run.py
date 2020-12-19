d993da3c292f545e8017e1326d855631f2f29a43
ludomitch <ludomitch198@gmail.com>
2020-12-19 19:48:37 +0000

    -

--- neuro/parallel_run.py
+++ neuro/parallel_run.py
@@ -48,7 +48,7 @@ def run(opt):
 		"training_set/red_maze.yml":1,
 		"training_set/wall.yml":3,
 		"training_set/choice.yml":2,
-		"training_set/numerosity.yml": 1,
+		"training_set/numerosity.yml": 10,
 		"training_set/ymaze.yml": 0
 	}
 	args = margs(
